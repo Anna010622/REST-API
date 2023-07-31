f97c899a22368e94e63305f4c16365fb185bca56
Anna Trifonova <3809322999@ukr.net>
2023-07-31 17:24:05 +0300

    Add decorators and middlewars

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,13 +6,26 @@ import {
 	deleteById,
 	updateById,
 } from '../../controllers/contacts-controller.js';
+import { ctrlWrapper, validateBody } from '../../decorators/index.js';
+import contactSchema from '../../schemas/contact-schema.js';
+import { isEmptyBody } from '../../middlewares/index.js';
 
 const contactsRouter = express.Router();
 
-contactsRouter.get('/', getAll);
-contactsRouter.get('/:contactId', getById);
-contactsRouter.post('/', add);
-contactsRouter.delete('/:contactId', deleteById);
-contactsRouter.put('/:contactId', updateById);
+contactsRouter.get('/', ctrlWrapper(getAll));
+contactsRouter.get('/:contactId', ctrlWrapper(getById));
+contactsRouter.post(
+	'/',
+	isEmptyBody,
+	validateBody(contactSchema),
+	ctrlWrapper(add)
+);
+contactsRouter.delete('/:contactId', ctrlWrapper(deleteById));
+contactsRouter.put(
+	'/:contactId',
+	isEmptyBody,
+	validateBody(contactSchema),
+	ctrlWrapper(updateById)
+);
 
 export default contactsRouter;
